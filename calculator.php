cbf79ffc11aef373232510989facc40af8790f56
MD Tofayel Karim Nirob <60335423+tofayal@users.noreply.github.com>
2021-11-22 07:20:29 -0800

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Answer</title>
+</head>
+<body>
+<p>The answer is: 
+<?php
+if($_POST['group1'] == add) {
+echo "$first + $second";
+}
+else if($_POST['group1'] == subtract) {
+echo "$first - $second";
+}
+else if($_POST['group1'] == times) {
+echo "$first * $second";
+}
+else($_POST['group1'] == divide) {
+echo "$first / $second";
+}
+?>
+</p> 
+</body>
+</html>
